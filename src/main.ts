460c85a24b9a70bce055d14ccc0669ec8e234cff
videsh.hegde <videsh.hegde@ibm.com>
2021-06-07 13:14:55 +0530

    Helmet added

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { CustomExceptionFilter } from './filters/exception-filter';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomExceptionFilter());
+  app.use(helmet());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
